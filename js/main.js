46c02514846270aa013b85a52914d260f538423f
Jarod Guthrie <jarod.guthrie@gmail.com>
2017-08-10 19:21:40 +1000

    Mobile resposive
    Updated header responsiveness for mobile. Changed top to 25%. Still not perf for ipad.
    Added detail sections at bottom of page

--- js/main.js
+++ js/main.js
@@ -265,17 +265,6 @@ $(document).ready(function(){
     /* 5. Newsletter ........................ */
     /* ------------------------------------- */
 
-    $("#notifyMe").notifyMe();
-
-    (function() {
-
-        var dlgtrigger = document.querySelector( '[data-dialog]' ),
-            somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
-            dlg = new DialogFx( somedialog );
-
-        dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
-
-    })();
 
     /* ------------------------------------- */
     /* 6. PhotoSwipe Gallery Init .......... */
